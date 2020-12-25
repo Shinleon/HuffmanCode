addfe910c8bad471493ea2d83b980f0c88691da4
shinleon <shinleon@uw.edu>
2020-12-24 21:12:21 -0800

    still don't understand how operator works,but we will persist

--- Attempt2/test.cpp
+++ Attempt2/test.cpp
@@ -1,4 +1,6 @@
 #include <cmath>
+#include <string>
+#include <iostream>
 
 class Point2D
 {
@@ -7,6 +9,8 @@ class Point2D
     double y;
 
   public:
+    Point2D(){this->x = 0; this->y = 0;}
+    Point2D (double x, double y){this->x = x; this->y = y;}
     double getX(){return this->x;}
     void setX(double input){this->x = input;}
     double getY(){return this->y;}
@@ -17,12 +21,24 @@ class Point2D
       double sqrdy = pow(this->y - other->getY(), 2);
       return sqrt(sqrdx + sqrdy);
     }
+    Point2D operator +(Point2D& other)
+    {
+      Point2D ret;
+      ret.x = other.x + this->x;
+      ret.y = other.y + this->y;
+      return ret;
+    }
+    std::string toString()
+    {
+      return "X: " + std::to_string(this->x) + "\tY: " + std::to_string(this->y);
+    }
 
 };
 
 int main()
 {
-  Point2D** x = new Point2D*;
-  *x = nullptr;
-
+  Point2D A(3, 4);
+  Point2D* B = new Point2D(12, 5);
+  Point2D C = (A) + *B;
+  std::cout << C.toString() + "\n" ;
 }
